7110db8c1d2f706feaa60cc87e0ddde6c1e58e4d
mohammed-rharbi <rharbi383@gmail.com>
2025-02-11 15:25:54 +0100

    fix(layout): update main app layout

--- smart-warehouse/app/_layout.tsx
+++ smart-warehouse/app/_layout.tsx
@@ -5,6 +5,7 @@ import * as SplashScreen from 'expo-splash-screen';
 import { StatusBar } from 'expo-status-bar';
 import { useEffect } from 'react';
 import 'react-native-reanimated';
+import { AuthProvider } from '@/context/authContext';
 
 import { useColorScheme } from '@/hooks/useColorScheme';
 
@@ -28,12 +29,15 @@ export default function RootLayout() {
   }
 
   return (
+
+    <AuthProvider>
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="(tabs)" options={{ headerShown: false}}/>
         <Stack.Screen name="+not-found" />
       </Stack>
       <StatusBar style="auto" />
     </ThemeProvider>
+    </AuthProvider>
   );
 }
