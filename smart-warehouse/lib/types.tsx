7dd2cd68e880b406295f42e85447c632feb01dfe
mohammed-rharbi <rharbi383@gmail.com>
2025-02-12 09:02:50 +0100

    chore(types): update type definitions

--- smart-warehouse/lib/types.tsx
+++ smart-warehouse/lib/types.tsx
@@ -11,3 +11,28 @@ export interface User{
     secretKey:string,
     warehouseId:string
 }
+
+
+
+export interface Product{
+    id:string,
+    name:string,
+    type:string,
+    barcode:string,
+    price:string,
+    solde:string,
+    supplier:string,
+    image:string,
+    stocks:[
+        {
+            id:string,
+            name:string,
+            quantity:string,
+            localisation:{
+                city:string ,
+                latitude: string ,
+                longitude: string },
+
+        }
+    ]
+}
