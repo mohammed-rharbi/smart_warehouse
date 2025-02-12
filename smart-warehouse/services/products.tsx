5e5490cb1dd7896d37546d65ebd98cbe91611f0c
mohammed-rharbi <rharbi383@gmail.com>
2025-02-12 17:28:36 +0100

    fix(services): update product service logic

--- smart-warehouse/services/products.tsx
+++ smart-warehouse/services/products.tsx
@@ -1,4 +1,5 @@
 import apiClient from "@/lib/apiClient";
+import { Product } from "@/lib/types";
 
 
 
@@ -36,4 +37,46 @@ export const fetchSinglProduct = async (id: string) => {
     } catch (error) { 
         throw new Error(' error while fetching the products');
     }
-};
+};  
+
+
+export const create = async (data : Product)=>{
+
+
+    try{
+
+        const res = await apiClient.post('products', data);
+
+        if(res.status === 200){
+
+            return res.data
+
+        }else {
+            throw new Error('product cannot create ');
+        }
+
+    }catch(error){
+        throw new Error(' error while creating the product');
+    }
+}
+
+
+export const getProductByBarcode = async (code : string)=>{
+
+
+    try{
+
+        const res = await apiClient.get(`products/?barcode=${code}`);
+
+        if(res.status === 200){
+
+            return res.data[0]
+
+        }else {
+            throw new Error('not product ben found ');
+        }
+
+    }catch(error){
+        throw new Error(' error while fetching the product');
+    }
+}
